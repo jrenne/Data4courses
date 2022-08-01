2c8f9ec83bc324ae34af09582873a90bbe1c04e4
jrenne <jprenne@gmail.com>
2022-08-01 07:45:31 +0200

    update

--- parapluie/parapluie.R
+++ parapluie/parapluie.R
@@ -10,16 +10,16 @@ Precipitations <- read.csv("xls_Climatologie1104.csv")
 
 Google$Semaine <- substr(Google$Semaine,1,10)
 Google$Semaine <- as.Date(Google$Semaine,"%Y-%m-%d")
-Google$mois <- as.integer(format(Google$Semaine,"%m"))
-Google$annee <- as.integer(format(Google$Semaine,"%Y"))
+Google$mois    <- as.integer(format(Google$Semaine,"%m"))
+Google$annee   <- as.integer(format(Google$Semaine,"%Y"))
 
-Precipitations$Mois <- as.Date(Precipitations$Mois,"%d/%m/%y")
+Precipitations$Mois   <- as.Date(Precipitations$Mois,"%d/%m/%y")
 Precipitations$precip <- apply(Precipitations[,2:7],1,mean)
 
 All.data <- data.frame(date = Precipitations$Mois,precip=Precipitations$precip)
 N <- dim(All.data)[1]
 
-# Add variable "parapluie" in All.data
+# Add variable "parapluie" (Google) in All.data
 All.data$parapluie <- NaN
 count <- 0
 for(a in 2005:2010){
@@ -30,33 +30,21 @@ for(a in 2005:2010){
   }
 }
 
-plot(All.data$date,All.data$parapluie,type='l')
-lines(All.data$date,All.data$precip,col='red')
-
+# Remove trend from Google data:
 trend <- 1:N
-eq <- lm(All.data$parapluie~All.data$precip + trend)
-summary((eq))
-
-
-# Detrend Google data:
 eq.trend <- lm(All.data$parapluie~trend)
-detrend.parapluie <- All.data$parapluie - eq.trend$coefficients[2]*trend
+detrend.parapluie <- eq.trend$residuals
 All.data$parapluie <- detrend.parapluie
-
-plot(All.data$date,All.data$parapluie,type='l')
-lines(All.data$date,All.data$precip,col='red')
 
 eq <- lm(All.data$parapluie~All.data$precip)
 summary((eq))
 
+plot(All.data$date,All.data$parapluie,type='l')
+lines(All.data$date,eq$fitted.values,col='red')
 
-par(mfrow=c(1,1))
-par(plt=c(.1,.95,.15,.95))
-plot(All.data$date,All.data$precip,lwd=2,type="l",xlab="",ylab="",ylim=c(20,200))
-par(new=TRUE)
-par(new=TRUE)
-plot(All.data$date,All.data$parapluie,lwd=2,col="red",type="l",
-     xlab="",ylab="",xaxt="n",yaxt="n")
+write.csv(All.data,file="../data4parapluie.csv",row.names=FALSE)
+
+stop()
 
 
 # Construct monthly dummies:
